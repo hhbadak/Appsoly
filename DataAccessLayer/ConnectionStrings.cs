524227ec47c9f4cacbf60b9260efa921c3634046
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2024-10-29 17:49:41 +0300

    is continued

--- DataAccessLayer/ConnectionStrings.cs
+++ DataAccessLayer/ConnectionStrings.cs
@@ -8,10 +8,10 @@ namespace DataAccessLayer
 {
     public class ConnectionStrings
     {
-        public static string ConStr = @"Data Source=.\SQLEXPRESS; Initial Catalog=Appsoly;Integrated Security=True";
+        //public static string ConStr = @"Data Source=.\SQLEXPRESS; Initial Catalog=Appsoly;Integrated Security=True";
 
         //public static string ConStr = @"Data Source=neptune.odeaweb.com;Network Library=DBMSSOCN;Initial Catalog=appsoly;User ID=appsoly;Password=60hDp5?8k";
 
-        //public static string ConStr = @"Data Source=mssql03.trwww.com;Network Library=DBMSSOCN;Initial Catalog=dpazaric_;User ID=appsolyWeb;Password=fa251$2Ba";
+        public static string ConStr = @"Data Source=mssql03.trwww.com;Network Library=DBMSSOCN;Initial Catalog=dpazaric_;User ID=appsolyWeb;Password=fa251$2Ba";
     }
 }
